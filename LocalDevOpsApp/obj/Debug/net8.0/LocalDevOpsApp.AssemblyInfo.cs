92ffd6360e9931b996c880bee29db114d9ceb9b2
Hakan Yaman <hakan.yaman+duzey@duzey.com.tr>
2026-01-20 16:29:45 +0300

    Update assembly informational version and editorconfig settings; remove Swagger configuration

--- LocalDevOpsApp/obj/Debug/net8.0/LocalDevOpsApp.AssemblyInfo.cs
+++ LocalDevOpsApp/obj/Debug/net8.0/LocalDevOpsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LocalDevOpsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+293ee554c107c7a435dbf0fa753459b94954ebe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("LocalDevOpsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LocalDevOpsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
